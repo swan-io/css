0512907a6a4d7255a745bb7ce47d4a315dc1bb24
Mathieu Acthernoene <zoontek@gmail.com>
2025-08-18 02:01:50 +0200

    Only export cx mjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "tsup";
 
 const config = {
-  entry: ["src/index.ts", "src/cx.ts", "src/vite-plugin.ts"],
+  entry: ["src/index.ts", "src/vite-plugin.ts"],
   target: "es2019",
   tsconfig: "./tsconfig.build.json",
   bundle: true,
   clean: false,
+  dts: false,
   sourcemap: false,
   splitting: false,
 };
 
 export default defineConfig([
   { ...config, format: "cjs", dts: true },
-  { ...config, format: "esm", dts: false },
+  { ...config, format: "esm" },
+  { ...config, entry: ["src/cx.ts"], format: "esm" },
 ]);
